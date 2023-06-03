0ad5fc1c0c8d3acc2390d02eae51562d843e0516
Robert <roberto.arevalocabello@gmail.com>
2023-06-03 19:26:42 -0300

    14

--- Guia11EjercicioExtra2B/src/Main/Main.java
+++ Guia11EjercicioExtra2B/src/Main/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package guia11ejercicioextra2b;
+package Main;
 
 import Entidad.Cine;
 import Entidad.Espectador;
