a60af24a38bcae66da93d51ddc21e7a0b7ed0678
Robert <roberto.arevalocabello@gmail.com>
2023-06-10 07:38:33 -0300

    15

--- Guia12Ejercicio2/src/Main/Main.java
+++ Guia12Ejercicio2/src/Main/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package guia12ejercicio2;
+package Main;
 
 import Entidad.Lavadora;
 import Entidad.Televisor;
