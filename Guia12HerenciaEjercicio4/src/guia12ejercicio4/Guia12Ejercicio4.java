610003ec92290f758dfb3e19ff41f6b5ab590768
Robert <roberto.arevalocabello@gmail.com>
2023-06-20 11:13:01 -0300

    6

---
+++ Guia12HerenciaEjercicio4/src/guia12ejercicio4/Guia12Ejercicio4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia12ejercicio4;
+
+/**
+ *
+ * @author Robert Are "BlackDragon" <https://github.com/Are851>
+ *   <https://www.linkedin.com/in/robert-arevalo-2438a9277/>
+ */
+public class Guia12Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
