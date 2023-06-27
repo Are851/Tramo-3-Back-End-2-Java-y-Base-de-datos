7ffba9aa8478f37b98914e03f29819c9df7aa4e4
Robert <roberto.arevalocabello@gmail.com>
2023-06-27 19:10:36 -0300

    8

--- Guia13ExcepcionesEjercicio7/src/Repuestas/Respuestas.java
+++ Guia13ExcepcionesEjercicio7/src/Repuestas/Respuestas.java
@@ -11,7 +11,6 @@ package Repuestas;
  *   <https://www.linkedin.com/in/robert-arevalo-2438a9277/>
  */
 public class Respuestas {
-
     /**
 class B {
 public void metodoB() {
@@ -31,18 +30,19 @@ Si se produce la excepción MioException en el bloque de código dentro del try,
 
 sentencia_b1: Se ejecuta como la primera sentencia fuera del bloque try.
 sentencia_b2: Se ejecuta dentro del bloque try.
-sentencia_b3: Se ejecuta si se produce la excepción MioException, es decir, si se lanza dicha excepción en el bloque try.
-sentencia_b4: Se ejecuta después del bloque try-catch, independientemente de si se produjo una excepción o no.
-b) ¿Qué sentencias y en qué orden se ejecutan si no se produce la excepción MioException?
-Si no se produce la excepción MioException en el bloque de
-     * código dentro del try, las sentencias se ejecutan en el siguiente orden:
+sentencia_b3: Se ejecuta si se produce la excepción MioException, es decir, si 
+se lanza dicha excepción en el bloque try.
+sentencia_b4: Se ejecuta después del bloque try-catch, independientemente de si 
+se produjo una excepción o no.
+b) ¿Qué sentencias y en qué orden se ejecutan si no se produce la excepción 
+MioException?
+Si no se produce la excepción MioException en el bloque de código dentro del 
+try, las sentencias se ejecutan en el siguiente orden:
      * sentencia_b1: Se ejecuta como la primera sentencia fuera del bloque try.
      * sentencia_b2: Se ejecuta dentro del bloque try. sentencia_b4: Se ejecuta
      * después del bloque try, ya que no se produjo ninguna excepción en el
      * bloque try. El bloque catch se omitirá en este caso, ya que no se lanzó
      * la excepción MioException.
-
-
 */
     
     }
